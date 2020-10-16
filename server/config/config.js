21966a5cb36f615d427bca9100486395cfb1cf0e
rolando <rolandojavier.ramos@gmail.com>
2020-10-16 08:51:22 -0500

    Agregamos una variable de entorno para heroku

--- server/config/config.js
+++ server/config/config.js
@@ -11,6 +11,6 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = "mongodb://localhost:27017/cafe";
 }else{
-    urlDB = "mongodb+srv://rolando:rolando123@cluster0.i9h1x.mongodb.net/cafe?retryWrites=true&w=majority";
+    urlDB = process.env.MONGO_URI;
 }
 process.env.URLDB = urlDB;
